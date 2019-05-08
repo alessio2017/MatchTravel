df698cfd63b49eded206e0f7a4feca4a1644d349
alessiofiorenza <alessio.fior@gmail.com>
2019-05-08 16:18:24 +0200

    Completato l'operazione per effettuare un nuovo survey

--- app/src/main/java/fragments/SurveyFragment.java
+++ app/src/main/java/fragments/SurveyFragment.java
@@ -1,7 +1,9 @@
 package fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,13 +11,25 @@ import android.view.ViewGroup;
 import javax.annotation.Nullable;
 
 import matchtravel.com.matchtravel.R;
+import matchtravel.com.matchtravel.SurveyActivity;
+import matchtravel.com.matchtravel.SurveyPageAdapter;
+import objectBoxUtility.SurveyManager;
 
 public class SurveyFragment extends Fragment {
+    private SurveyManager sur;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_survey, container, false);
+        View view = inflater.inflate(R.layout.fragment_survey, container, false);
+        ViewPager viewPager = view.findViewById(R.id.pager);
+        // Create an adapter that knows which fragment should be shown on each page
+        this.sur = new SurveyManager();
+        SurveyPageAdapter adapterTab = new SurveyPageAdapter(getActivity().getSupportFragmentManager(), sur);
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapterTab);
+        viewPager.setOffscreenPageLimit(2);
+        return view;
     }
 
 }
