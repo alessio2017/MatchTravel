e233d927e0c0e674915fa6d84dfd7c0262fb5e61
Daniele <daniele.buonadonna95@gmail.com>
2019-05-08 12:41:50 +0200

    La recyclerView del fragment "home" è correttamente inizializzata. Tuttavia non è ancora stato possibile analizzare bene il layout finale in quanto mancano alcuni dati. Probabilmente si dovranno fare delle correzioni alla fine.

--- app/src/main/java/fragments/HomeFragment.java
+++ app/src/main/java/fragments/HomeFragment.java
@@ -2,30 +2,78 @@ package fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.Nullable;
 
+import datadb.City;
 import datadb.User;
 import matchtravel.com.matchtravel.R;
+import objectBoxUtility.ObjectBox;
+import objectBoxUtility.UserManager;
+import wishlistUtility.WishlistAdapter;
 
 public class HomeFragment extends Fragment {
 
     private User currentUser;
+    private View layout;
+    private WishlistAdapter recyclerViewAdapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-        //TODO: caricare lista delle città dell'utente corrente
+        this.layout = inflater.inflate(R.layout.fragment_home, container, false);
+        if(currentUser!=null)
+            updateRecyclerView();
+        return layout;
     }
 
     public void setCurrentUser(User user){
-        if(user!=null)
+        if(user!=null){
             this.currentUser = user;
-        //TODO: update list of cities
+            if(layout!=null)
+                updateRecyclerView();
+        }
+    }
+
+    private void updateRecyclerView(){
+        if(recyclerViewAdapter == null)
+            recyclerViewAdapter = new WishlistAdapter();
+
+        //giving to the adapter the list of city of the current user
+        recyclerViewAdapter.setCities(currentUser.destinations);
+
+        //list of user per city (of the current user)
+        Map<City, List<User>> cityUserMap = new HashMap<>();
+        UserManager userManager = new UserManager(ObjectBox.get());
+        for(City c : currentUser.destinations){
+            //building map
+            List<User> usersWithTheSameDestination = userManager.getUsersForCity(c);
+            cityUserMap.put(c, usersWithTheSameDestination);
+        }
+        recyclerViewAdapter.setUserPerCity(cityUserMap);
+        RecyclerView recyclerView;
+        try{
+            recyclerView = layout.findViewById(R.id.wishlistRecyclerView);
+            recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+            recyclerView.setAdapter(recyclerViewAdapter);
+            Log.i("HOME_FRAGMENT", "RecyclerView inizializzata con successo");
+        }catch (Exception e){
+            e.printStackTrace();
+            Log.i("HOME_FRAGMENT", "RecyclerView non inizializzata");
+        }
+
+        //notifying to the adapter that the set is changed
+        recyclerViewAdapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/wishlistUtility/WishlistViewHolder.java
@@ -0,0 +1,108 @@
+package wishlistUtility;
+
+import android.media.Image;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import datadb.City;
+import datadb.User;
+import matchtravel.com.matchtravel.R;
+
+public class WishlistViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView cityName;
+    private TextView user1Name;
+    private TextView user2Name;
+    private TextView user3Name;
+    private ImageView cityImage;
+    private ImageView user1Image;
+    private ImageView user2Image;
+    private ImageView user3Image;
+    private ImageButton contactUser1;
+    private ImageButton contactUser2;
+    private ImageButton contactUser3;
+    private ImageButton favouriteBtn;
+
+    public WishlistViewHolder(@NonNull View itemView) {
+        super(itemView);
+        this.cityName = itemView.findViewById(R.id.cityName);
+        this.user1Name = itemView.findViewById(R.id.userName1);
+        this.user2Name = itemView.findViewById(R.id.userName2);
+        this.user3Name = itemView.findViewById(R.id.userName3);
+        //image view
+        this.cityImage = itemView.findViewById(R.id.imageCity);
+        this.user1Image = itemView.findViewById(R.id.imageUser1);
+        this.user2Image = itemView.findViewById(R.id.imageUser2);
+        this.user3Image = itemView.findViewById(R.id.imageUser3);
+        //contact button
+        this.contactUser1 = itemView.findViewById(R.id.buttonGoToProfile1);
+        this.contactUser2 = itemView.findViewById(R.id.buttonGoToProfile2);
+        this.contactUser3 = itemView.findViewById(R.id.buttonGoToProfile3);
+        //star button
+        this.favouriteBtn = itemView.findViewById(R.id.buttonStar);
+    }
+
+    public void bindData(final City city, final List<User> users){
+        cityName.setText(city.getName());
+        //TODO: set city image
+        //TODO: set button star
+
+        String userName = "";
+
+        if(users.size()>0){
+            userName = users.get(0).getName() + "\n" + users.get(0).getSurname();
+            user1Name.setText(userName);
+            //TODO: set user image
+            if(users.size()>1){
+                userName = users.get(1).getName() + "\n" + users.get(1).getSurname();
+                user2Name.setText(userName);
+                //TODO: set user image
+                if(users.size()>2){
+                    userName = users.get(2).getName() + "\n" + users.get(2).getSurname();
+                    user3Name.setText(userName);
+                    //TODO: set user image
+                }
+                else{
+                    //hiding image and button
+                    user3Name.setVisibility(View.GONE);
+                    user3Image.setVisibility(View.GONE);
+                    contactUser3.setVisibility(View.GONE);
+                }
+            }
+            else{
+                //hiding image and button
+                user2Name.setVisibility(View.GONE);
+                user3Name.setVisibility(View.GONE);
+                user2Image.setVisibility(View.GONE);
+                user3Image.setVisibility(View.GONE);
+                contactUser2.setVisibility(View.GONE);
+                contactUser3.setVisibility(View.GONE);
+            }
+        }
+        else{
+            //hiding image and button
+            user1Name.setVisibility(View.GONE);
+            user2Name.setVisibility(View.GONE);
+            user3Name.setVisibility(View.GONE);
+            user1Image.setVisibility(View.GONE);
+            user2Image.setVisibility(View.GONE);
+            user3Image.setVisibility(View.GONE);
+            contactUser1.setVisibility(View.GONE);
+            contactUser2.setVisibility(View.GONE);
+            contactUser3.setVisibility(View.GONE);
+        }
+
+        this.favouriteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: rimuovere la città tra le preferite dell'utente, deve uscire un popup del tipo: "sei sicuro di..."
+            }
+        });
+    }
+}
