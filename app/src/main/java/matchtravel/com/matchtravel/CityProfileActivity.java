38c4776ea52f0ef1a9e3f9bb595a3d9e2e151ac2
umbertodicanito <umbertodicanito@gmail.com>
2019-05-09 20:25:18 +0200

    creazione del branch dedicato al profilo della singola città

---
+++ app/src/main/java/matchtravel/com/matchtravel/CityProfileActivity.java
@@ -0,0 +1,13 @@
+package matchtravel.com.matchtravel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CityProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_profile);
+    }
+}
